bb37680d784e77d28e5556eef4079229873ab5d5
thyago herique <01671368444@seplan.govrn>
2019-10-01 08:28:09 -0300

    commit inicial com algumas rotas do node

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+let apiRoutes = require("./api-routes");
+var port = process.env.PORT || 8080;
+
+app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => res.send('first page'));
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+})
